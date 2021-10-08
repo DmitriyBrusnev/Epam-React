2e79e18853f6158d6db630684f6054dd34baa54f
Dmitry Brusnev <dmitry.brusnev@kidskey.org>
2021-10-08 12:50:00 +0300

    task2: ready to test

--- task1/src/components/content/Content.jsx
+++ task1/src/components/content/Content.jsx
@@ -2,23 +2,75 @@ import React, { useState, useEffect } from 'react';
 
 import './content.scss';
 
-function Content(props) {
-    const [text, setText] = useState('');
-    const goalText = 'Content...';
+class Content extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        albums: []
+      };
+    }
+  
+    componentDidMount() {
+      fetch("https://jsonplaceholder.typicode.com/albums")
+        .then(res => res.json())
+        .then(
+          (result) => {
+              console.log(result)
+            this.setState({
+              isLoaded: true,
+              albums: result
+            });
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+    }
+  
+    render() {
+      const { error, isLoaded, albums } = this.state;
+      if (error) {
+        return <div>Error: { error.message }</div>;
+      } else if (!isLoaded) {
+        return <div>Loading...</div>;
+      } else {
+        return (
+          <ul className='albums'>
+            {
+                albums.map(album => (
+                <li key={ album.id } className='albums__item'>
+                    Title - <span className='album__title'>{ album.title }</span>
+                </li>
+                ))
+            }
+          </ul>
+        );
+      }
+    }
+  }
 
-    useEffect(() => {
-        for (let i = 0; i < goalText.length; i++) {
-            setTimeout(() => {
-                setText((prevText) => prevText + goalText[i]);
-            }, 2000 * i);
-        }
-    }, []);
+// function Content(props) {
+//     const [text, setText] = useState('');
+//     const goalText = 'Content...';
 
-    return (
-        <div className="content">
-            <span className="anim-typewriter">{ text }</span>
-        </div>
-    );
-}
+//     useEffect(() => {
+//         for (let i = 0; i < goalText.length; i++) {
+//             setTimeout(() => {
+//                 setText((prevText) => prevText + goalText[i]);
+//             }, 2000 * i);
+//         }
+//     }, []);
+
+//     return (
+//         <div className="content">
+//             <span className="anim-typewriter">{ text }</span>
+//         </div>
+//     );
+// }
 
 export default Content;
