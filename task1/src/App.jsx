e0676caacc69f174a5549917b08961a8e7da2ae2
Dmitry Brusnev <dmitry.brusnev@kidskey.org>
2021-11-03 15:26:45 +0300

    task6: wip

--- task1/src/App.jsx
+++ task1/src/App.jsx
@@ -1,4 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+
+
 import Content from './components/content/Content';
 import Details from './components/user/Details';
 import { Provider } from 'react-redux';
@@ -6,6 +12,7 @@ import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './redux/reducer/rootReducer';
 import thunk from 'redux-thunk';
 import { ErrorBoundary } from './components/error/ErrorBoundary.jsx';
+import Login from './components/login/Login';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
@@ -13,10 +20,15 @@ function App(props) {
   return (
     <ErrorBoundary>
       <Provider store={ store }>
-        <div className="container">
-          <Details { ...props } />
-          <Content />
-        </div>
+        <Router>
+          <div className="container">
+            <Route path="/login" exact>
+              <Login />
+            </Route>
+            <Details { ...props } />
+            <Content />
+          </div>
+        </Router>
       </Provider>
     </ErrorBoundary>
   );
